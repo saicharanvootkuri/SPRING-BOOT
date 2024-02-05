114ec41c80b046a11fe057ac742b874e8100c95c
saicharanvootkuri <vootkurisaicharan@gmail.com>
2024-02-05 18:11:34 +0530

    commit

--- studentmanagement/src/main/java/com/Student/springboot/studentmanagement/StudentmanagementApplication.java
+++ studentmanagement/src/main/java/com/Student/springboot/studentmanagement/StudentmanagementApplication.java
@@ -2,7 +2,13 @@ package com.Student.springboot.studentmanagement;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+@EnableJpaRepositories
+@EnableTransactionManagement
+@EnableJpaAuditing
 @SpringBootApplication
 public class StudentmanagementApplication {
 
--- studentmanagement/src/main/java/com/Student/springboot/studentmanagement/entity/StudentEntity.java
+++ studentmanagement/src/main/java/com/Student/springboot/studentmanagement/entity/StudentEntity.java
@@ -1,13 +1,24 @@
 package com.Student.springboot.studentmanagement.entity;
 
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
-
-//@Data
+import lombok.Data;
+@EntityListeners(AuditingEntityListener.class)
+@Data
 @Entity(name = "student")
 @Table(name = "students")
 public class StudentEntity {
@@ -29,57 +40,56 @@ public class StudentEntity {
 	@Column(name = "mobile_number")
 	private String mobileNumber;
 
-	public StudentEntity() {
 
-	}
+@JsonFormat(pattern="MM/dd/yyyy HH:mm")
+@DateTimeFormat(pattern = "MM/dd/yyyy HH:mm")
+@CreatedDate
+private LocalDateTime createdDate;
 
-	public StudentEntity(long id, String firstName, String lastName, String studentIdNumber, String branch,
-			String mobileNumber) {
+@JsonFormat(pattern="MM/dd/yyyy HH:mm")
+@DateTimeFormat(pattern = "MM/dd/yyyy HH:mm")
+@LastModifiedDate
+private LocalDateTime lastModifiedDate;
 
-		this.id = id;
-		this.firstName = firstName;
-		this.lastName = lastName;
-		this.branch = branch;
-		this.mobileNumber = mobileNumber;
-	}
-
-	public long getId() {
-		return id;
-	}
-
-	public void setId(long id) {
-		this.id = id;
-	}
-
-	public String getFirstName() {
-		return firstName;
-	}
-
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-
-	public String getLastName() {
-		return lastName;
-	}
-
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
-
-	public String getBranch() {
-		return branch;
-	}
-
-	public void setBranch(String branch) {
-		this.branch = branch;
-	}
-
-	public String getMobileNumber() {
-		return mobileNumber;
-	}
-
-	public void setMobileNumber(String mobileNumber) {
-		this.mobileNumber = mobileNumber;
-	}
 }
+
+/*
+ * public StudentEntity() {
+ * 
+ * }
+ * 
+ * public StudentEntity(long id, String firstName, String lastName, String
+ * studentIdNumber, String branch, String mobileNumber) {
+ * 
+ * this.id = id; this.firstName = firstName; this.lastName = lastName;
+ * this.branch = branch; this.mobileNumber = mobileNumber; }
+ * 
+ * public long getId() { return id; }
+ * 
+ * public void setId(long id) { this.id = id; }
+ * 
+ * public String getFirstName() { return firstName; }
+ * 
+ * public void setFirstName(String firstName) { this.firstName = firstName; }
+ * 
+ * public String getLastName() { return lastName; }
+ * 
+ * public void setLastName(String lastName) { this.lastName = lastName; }
+ * 
+ * public String getBranch() { return branch; }
+ * 
+ * public void setBranch(String branch) { this.branch = branch; }
+ * 
+ * public String getMobileNumber() { return mobileNumber; }
+ * 
+ * public void setMobileNumber(String mobileNumber) { this.mobileNumber =
+ * mobileNumber; }
+ * 
+ * // public LocalDateTime getCreatedAt() { // return createdAt; // } // //
+ * public void setCreatedAt(LocalDateTime createdAt) { // this.createdAt =
+ * createdAt; // } // // // public LocalDateTime getUpdatedAt() { // return
+ * updatedAt; // } // // public void setUpdatedAt(LocalDateTime updatedAt) { //
+ * this.updatedAt = updatedAt; // }
+ * 
+ * }
+ */
